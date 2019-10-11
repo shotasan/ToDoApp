21f2bf31cb4b8bbca5db5cf698290c412de18742
shotasan <wondermoai@gmail.com>
2019-10-11 23:20:37 +0900

    Add: タスク追加機能を実装

--- ToDoApp.js
+++ ToDoApp.js
@@ -1,11 +1,18 @@
 var app = new Vue({
   el: "#app",
   data: {
-    message: "Hello Vue.js"
+    newItem: "",
+    todos: []
   },
   methods: {
     addItem: function (event) {
+      // データバインディングで入力されたdata.newItemをオブジェクトの値とする
+      var todo = {
+        item: this.newItem
+      };
 
+      // 空の配列にオブジェクトtodo(keyがitem, valueがnewItem)を格納する
+      this.todos.push(todo);
     }
   }
 })
