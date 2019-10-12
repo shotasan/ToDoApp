f81a208b6dbbb6d43feae1f32055a0adff193d0a
shotasan <wondermoai@gmail.com>
2019-10-12 12:14:36 +0900

    Add: タスクのチェック機能、削除機能を実装

--- ToDoApp.js
+++ ToDoApp.js
@@ -6,13 +6,25 @@ var app = new Vue({
   },
   methods: {
     addItem: function (event) {
+      // 入力欄が空の場合は処理を終了する
+      if (this.newItem == "") return;
+
       // データバインディングで入力されたdata.newItemをオブジェクトの値とする
       var todo = {
-        item: this.newItem
+        item: this.newItem,
+        isDone: false
       };
 
-      // 空の配列にオブジェクトtodo(keyがitem, valueがnewItem)を格納する
+      // 空の配列todosにオブジェクトtodo(keyがitem, valueがnewItem)を格納する
       this.todos.push(todo);
+
+      // 入力欄をクリアする処理
+      this.newItem = "";
+    },
+
+    // タスクの削除機能
+    deleteItem: function (index) {
+      this.todos.splice(index, 1);
     }
   }
 })
